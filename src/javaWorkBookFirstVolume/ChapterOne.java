96ba18526ffa49628d3e35700a04e05ff6f7cd97
JordanLaurent09 <bluff_eversmoking@inbox.ru>
2023-08-27 22:14:25 +0200

    using Scanner class

--- src/javaWorkBookFirstVolume/ChapterOne.java
+++ src/javaWorkBookFirstVolume/ChapterOne.java
@@ -1,10 +1,14 @@
 package javaWorkBookFirstVolume;
 import javax.swing.JOptionPane;
+import java.util.Scanner;
+
 
 public class ChapterOne {
 
 	public static void main(String[] args) {
 		JOptionPane.showMessageDialog(null, "First steps in swing");
+		
+		Scanner input = new Scanner(System.in);
 	}
 
 }
