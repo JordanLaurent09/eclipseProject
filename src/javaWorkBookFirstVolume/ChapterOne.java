ed78422a73c6243e643b347dc9b0e1c08594afac
JordanLaurent09 <bluff_eversmoking@inbox.ru>
2023-08-28 21:40:08 +0200

    showInputDialog

--- src/javaWorkBookFirstVolume/ChapterOne.java
+++ src/javaWorkBookFirstVolume/ChapterOne.java
@@ -6,14 +6,32 @@ import java.util.Scanner;
 public class ChapterOne {
 
 	public static void main(String[] args) {
+		// Creation of message dialog
 		JOptionPane.showMessageDialog(null, "First steps in swing");
 		
+		
+		// Creation Scanner class object for input
 		Scanner input = new Scanner(System.in);
 		
-		int age = input.nextInt();
-		String name = input.nextLine();
 		
-		JOptionPane.showMessageDialog(null, "My name is " + name + " and I " + age + " years old");
+		// Variables for input		
+		String name = input.nextLine();
+		int age = input.nextInt();
+		
+		String message = "My name is " + name + " and I " + age + " years old";
+		input.close();
+		
+		// Message dialog with input values
+		JOptionPane.showMessageDialog(null, message);
+		
+		
+		// Use showInputDialog() method
+		
+		String result;
+		
+		result = JOptionPane.showInputDialog(null, "What is your name?");
+		
+		JOptionPane.showMessageDialog(null, "Hello, " + result + ".");
 	}
 
 }
