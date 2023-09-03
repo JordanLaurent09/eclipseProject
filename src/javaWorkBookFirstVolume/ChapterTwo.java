89a7c9232c1b78347ae8efcdf8d3e947ecf1bee4
JordanLaurent09 <bluff_eversmoking@inbox.ru>
2023-09-03 21:49:21 +0200

    further explore of JOptionPane class

--- src/javaWorkBookFirstVolume/ChapterTwo.java
+++ src/javaWorkBookFirstVolume/ChapterTwo.java
@@ -5,11 +5,18 @@ public class ChapterTwo {
 	
 	public static void main(String[] args) {
 		
+		// Using showConfirmDialog method 
+		
 		int selection;
 		boolean isYes;
 		selection = JOptionPane.showConfirmDialog(null, "Are you friend or foe?");
 		isYes = (selection == JOptionPane.YES_OPTION);
 		JOptionPane.showMessageDialog(null, isYes);
+		
+		// Using showConfirmDialog to create error message
+		
+		JOptionPane.showConfirmDialog(null, "Input value has incorrect type. Will you continie?", "Error message", JOptionPane.YES_NO_OPTION, JOptionPane.ERROR_MESSAGE);
+		
 	}
 
 }
